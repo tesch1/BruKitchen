9a3d03858d0048f2926f952e677f604a6e5a5236
Michael Tesch <tesch1@gmail.com>
2016-10-27 12:52:18 +0200

    reduce compile requirement to c++11

--- matlab/brukitchen_setup.m
+++ matlab/brukitchen_setup.m
@@ -1,3 +1,3 @@
 
-mex mexldr.cpp jcampdx.cpp jcamp_parse.cpp jcamp_scan.cpp FileLoc.cpp CXXFLAGS="-std=c++14 -fPIC"
+mex mexldr.cpp jcampdx.cpp jcamp_parse.cpp jcamp_scan.cpp FileLoc.cpp CXXFLAGS="-std=c++11 -fPIC"
 
